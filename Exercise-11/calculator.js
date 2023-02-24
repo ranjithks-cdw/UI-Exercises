286d40b1b32b91a74970296abca7090d00707dd9
ranjithks-cdw <ranjith.saravanan@cdw.com>
2023-02-24 18:05:57 +0530

    feat: Added comments

--- Exercise-11/calculator.js
+++ Exercise-11/calculator.js
@@ -1,18 +1,26 @@
 let Calculator = {
+    // Addition Function
     add : function(value1,value2) {
         return value1+value2;
     },
+
+    // Subtraction Function
     sub : function(value1,value2) {
         return value1-value2;
     },
+
+    // Multiplication Function
     mul : function(value1,value2) {
         return value1*value2;
     },
+
+    // Division Function
     div : function(value1,value2) {
         return value1/value2;
     }
 }
 
+// Calling Function
 console.log(Calculator.add(1,2));
 console.log(Calculator.sub(5,3));
 console.log(Calculator.mul(7,2));
