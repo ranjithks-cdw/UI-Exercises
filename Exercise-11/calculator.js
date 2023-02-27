3af3d9bd82a7743bb37ad39de51c5bda4e632bd8
ranjithks-cdw <ranjith.saravanan@cdw.com>
2023-02-27 17:03:07 +0530

    fix: Added validation to the methods

--- Exercise-11/calculator.js
+++ Exercise-11/calculator.js
@@ -1,27 +1,41 @@
-let Calculator = {
+Calculator = {
     // Addition Function
     add : function(value1,value2) {
+        if(!Calculator.validate(value1,value2))
+            return Calculator.validationErrorMsg;
         return value1+value2;
     },
 
     // Subtraction Function
     sub : function(value1,value2) {
+        if(!Calculator.validate(value1,value2))
+            return Calculator.validationErrorMsg;
         return value1-value2;
     },
 
     // Multiplication Function
     mul : function(value1,value2) {
+        if(!Calculator.validate(value1,value2))
+            return Calculator.validationErrorMsg;
         return value1*value2;
     },
 
     // Division Function
     div : function(value1,value2) {
+        if(!Calculator.validate(value1,value2))
+            return Calculator.validationErrorMsg;
         return value1/value2;
-    }
-}
+    },
+    validate : function(value1,value2) {
+        return typeof(value1)==typeof(value2) && typeof(value1)=="number";
+    },
+    validationErrorMsg : "Enter proper values to perform arithmetic operation"
+};
 
 // Calling Function
 console.log(Calculator.add(1,2));
 console.log(Calculator.sub(5,3));
 console.log(Calculator.mul(7,2));
 console.log(Calculator.div(7,19));
+console.log(Calculator.add('1',2));
+console.log(Calculator.div(2,0));
