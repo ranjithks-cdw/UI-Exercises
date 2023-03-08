29bbb3bd6585e5a2b1d12d4455515c8212e79848
ranjithks-cdw <ranjith.saravanan@cdw.com>
2023-03-08 10:43:32 +0530

    fix: Form validations

--- Exercise-14/js/app.js
+++ Exercise-14/js/app.js
@@ -1,56 +1,56 @@
-function validateInputForm() {
+const pattern = {
+    "firstName": /^[a-zA-z]{1,30}/,
+    "lastName": /^[a-zA-z]{1,30}/,
+    "email": /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    "contactNumber": /[6-9]\d{9}/,
+    "pinCode": /[1-9]\d{5}|[1-9]\d{2}\\s\d{3}/,
+    "cardNumber": /4[0-9]{15}/,
+    "cardExpiryYear": /202[3-9]/,
+    "cvv": /\d{3,4}/,
+};
+
+(function(){
+    const submitButton = document.getElementById("submitBtn").addEventListener("click",validateInputForm);
+})();
+
+function validateInputForm(event) {
+    event.preventDefault();
     const anyIncorrectDetails = [];
     // Form Inputs
-    let firstName = document.getElementById("firstName");
-    let lastName = document.getElementById("lastName");
-    let email = document.getElementById("email");
-    let contactNumber = document.getElementById("contactNumber");
-    let pinCode = document.getElementById("pinCode");
-    let cardNumber = document.getElementById("cardNumber");
-    let expiryYear = document.getElementById("cardExpiryYear");
-    let cvv = document.getElementById("cvv");
-
-    // Patterns
-    let namePattern =  /[a-zA-Z]{1,30}/;
-    let emailPattern = /(?=^.{0,50}$)(^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$)/;
-    let contactNumberPattern = /[6-9]\d{9}/;
-    let pinCodePattern = /[1-9]\d{5}|[1-9]\d{2}\\s\d{3}/;
-    let cardNumberPattern = /^4[0-9]{12}(?:[0-9]{3})?$/;
-    let expiryYearPattern = /202[3-9]/;
-    let cvvPattern = /\d{3,4}/;
-
+    const inputElements = document.querySelectorAll("input");
     // Validations
-    anyIncorrectDetails.push(validateValuesWithPattern(firstName,namePattern,"First Name","firstNameError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(lastName,namePattern,"Last Name","lastNameError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(email,emailPattern,"Email Address","emailError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(contactNumber,contactNumberPattern,"Contact Number","contactNumberError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(pinCode,pinCodePattern,"PIN Code","pinCodeError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(cardNumber,cardNumberPattern,"Card Number","cardNumberError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(expiryYear,expiryYearPattern,"Card Expiry","cardExpiryYearError"));
-    anyIncorrectDetails.push(validateValuesWithPattern(cvv,cvvPattern,"CVV","cvvError"));
+    for(let field of inputElements)
+        anyIncorrectDetails.push(validateValuesWithPattern(field));
 
-    if(!anyIncorrectDetails.includes(true))
-        location.href = "success.html";
+    if(!anyIncorrectDetails.includes(true)) {
+        return true;
+    }
     return false;
 }
 
-function validateValuesWithPattern(inputDOM,pattern,fieldName,errorId) {
+function validateValuesWithPattern(inputDOM) {
     let isError = false;
     let errorMessage = "";
+    const errorMessageDOM = document.getElementById(`${inputDOM.id}Error`);
 
-    if(!pattern.test(inputDOM.value)) {
+    if(!pattern[inputDOM.id].test(inputDOM.value)) {
         isError = true;
-        errorMessage = fieldName+" is not valid";
+        errorMessage = `${inputDOM.name} is not valid`;
     }
 
     if(inputDOM.value=="") {
+        console.log(inputDOM.value);
         isError = true;
-        errorMessage = fieldName+" is required";
+        errorMessage = `${inputDOM.name} is required`;
     }
 
     if(isError) {
-        document.getElementById(errorId).innerText = errorMessage;
+        errorMessageDOM.innerText = errorMessage;
         inputDOM.classList.add("error-input");
+    }
+    else {
+        errorMessageDOM.innerText = "";
+        inputDOM.classList.remove("error-input")
     }
     return isError;
 }
